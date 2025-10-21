5323d54a2a0d72eacfc1926084fb69c1d1173ee7
Christian Perez de Arce <christian@Mac-mini-de-Christian.local>
2025-10-21 15:50:48 -0300

    feat: add welcome and stackss information in academy page

--- academic-certificates/app/actions/academy.ts
+++ academic-certificates/app/actions/academy.ts
@@ -4,13 +4,14 @@ import { createClient } from "@/lib/supabase/server";
 export async function getAcademyCredentials(id: string): Promise<{
     stacksAddress: string;
     privateKey: string;
+    name: string;
 }> {
     console.log(id)
     const supabase = await createClient();
     console.log("Fetching academy credentials for ID:", id);
     const { data, error }: { data: any; error: any; } = await supabase
         .from("users")
-        .select("private_key,stacks_address ")
+        .select("private_key,stacks_address, nombre ")
         .eq("role", "academy")
         .eq("id_user", id)
         .maybeSingle();
@@ -19,14 +20,14 @@ export async function getAcademyCredentials(id: string): Promise<{
         throw new Error(error.message);
     }
 
-    return { stacksAddress: data.stacks_address, privateKey: data.private_key };
+    return { stacksAddress: data.stacks_address, privateKey: data.private_key, name: data.nombre };
 }
 
 export async function getStudentWallet(email: string) {
     const supabase = await createClient();
     const { data, error } = await supabase
         .from("users")
-        .select("stacks_address, id")
+        .select("stacks_address, id, nombre")
         .eq("role", "student")
         .eq("email", email)
         .maybeSingle();
