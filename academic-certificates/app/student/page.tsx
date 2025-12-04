f77200fb805e8593f70123f4fb2a40ab25336840
Christian Perez de Arce <christian@Mac-mini-de-Christian.local>
2025-12-04 18:43:12 -0300

    feat: add validation role user

--- academic-certificates/app/student/page.tsx
+++ academic-certificates/app/student/page.tsx
@@ -4,14 +4,16 @@ import StudentDashboard from "@/components/student-dashboard";
 
 export default async function StudentPage() {
   const supabase = await createClient();
-
-  const { data, error } = await supabase.auth.getClaims();
-  if (error || !data?.claims) {
+  const { data, error } = await supabase.auth.getUser();
+  if (error || !data?.user) {
+    redirect("/auth/login");
+  }
+  if (data.user.user_metadata.role !== "student") {
     redirect("/auth/login");
   }
   const dataUser = {
-    id: data.claims.sub,
-    email: data.claims.email,
+    id: data.user.id as string,
+    email: data.user.email as string,
   }
   return <StudentDashboard user={dataUser} />;
 }
